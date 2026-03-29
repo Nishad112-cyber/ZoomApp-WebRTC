d5acd55a89f2ebfaa462b05642a0d916b57652fd
Nishad112-cyber <nishadguptad@gmail.com>
2026-03-29 11:20:46 +0530

    videocomponent

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import Authentication from "./pages/authentication";
 import { AuthProvider } from "./Contexts/AuthProvider";
+import VideoMeetComponent from './pages/videoMeet';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/auth" element={<Authentication />} />
+           <Route path='/:url' element={<VideoMeetComponent />} />
         </Routes>
       </AuthProvider>
     </div>
